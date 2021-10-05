2e7ce16561e1334c3f397e3e6d2d38b6a92999a0
mizarion <zsgluschenko@edu.hse.ru>
2021-10-05 18:30:44 +0300

    Fix UDP connection

--- main.py
+++ main.py
@@ -19,30 +19,49 @@ def create_logger(file_name):
 
 def run_server(port, protocol, file_name):
     logger = create_logger(file_name).info
+    prot = 'UDP' if protocol == SOCK_DGRAM else 'TCP'
+    logger(f"Running {prot} server....")
     server_socket = socket(AF_INET, protocol)
     server_socket.bind(('', port))
-    server_socket.listen(1)
     logger("The server is ready to receive")
-    while 1:
-        connection_socket, addr = server_socket.accept()
-        logger("New client has connected. Start shipping.")
-        ip, port = connection_socket.getpeername()
-        connection_socket.send(ip.encode())
-        connection_socket.send(str(port).encode())
-        logger("Finished. Close the connection and wait for a new client.")
-        connection_socket.close()
+    if (protocol == SOCK_DGRAM):  # if UDP
+        while 1:
+            message, client_address = server_socket.recvfrom(2048)
+            logger(f"[{prot}_Server]: New client {client_address} has connected. Start shipping...")
+            ip, port = client_address
+            server_socket.sendto(ip.encode(), client_address)
+            server_socket.sendto(str(port).encode(), client_address)
+            logger(f"[{prot}_Server]: Finished. Wait for a new client.")
+    else:
+        server_socket.listen(1)
+        while 1:
+            connection_socket, addr = server_socket.accept()
+            ip, port = connection_socket.getpeername()
+            logger(f"[{prot}_Server]: New client ({ip}:{port}) has connected. Start shipping.")
+            connection_socket.send(ip.encode())
+            connection_socket.send(str(port).encode())
+            logger(f"[{prot}_Server]: Finished. Close the connection and wait for a new client.")
+            connection_socket.close()
 
 
 def run_client(host, port, protocol, file_name):
     logger = create_logger(file_name).info
     client_socket = socket(AF_INET, protocol)
-    client_socket.connect((host, port))
-    logger(f"[Client]: Connect to the {host}:{port}")
-    ip = client_socket.recv(1024).decode()
-    port = client_socket.recv(1024).decode()
-    logger(f'[Client]: Received a response from the server: {ip}:{port}')
-    client_socket.close()
-    logger(f"[Client]: Closed the connection")
+    prot = "UDP" if protocol == SOCK_DGRAM else "TCP"
+    if (protocol == SOCK_DGRAM):  # if UDP
+        client_socket.sendto('hi'.encode(), (host, port))
+        logger(f"[{prot}_Client]: Connect to the {host}:{port}")
+        ip = client_socket.recvfrom(1024)[0].decode()
+        port = client_socket.recvfrom(1024)[0].decode()
+        logger(f'[{prot}_Client]: Received a response from the server: {ip}:{port}')
+    else:
+        client_socket.connect((host, port))
+        logger(f"[{prot}_Client]: Connect to the {host}:{port}")
+        ip = client_socket.recv(1024).decode()
+        port = client_socket.recv(1024).decode()
+        logger(f'[{prot}_Client]: Received a response from the server: {ip}:{port}')
+        client_socket.close()
+        logger(f"[{prot}_Client]: Closed the connection")
 
 
 if __name__ == '__main__':
@@ -66,10 +85,11 @@ if __name__ == '__main__':
 
     # Run Server
     if (args.s):
-        try:
-            run_server(int(args.port), protocol, args.f)
-        except OSError:
-            print("Сервер с такими параметрами уже запущен")
+        # try:
+        # run_server(int(args.port), protocol, args.f)
+        run_server(int(args.port), protocol, args.f)
+        # except OSError:
+        #    print("Сервер с такими параметрами уже запущен")
 
     # Run Client
     else:
